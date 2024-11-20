6ec9560b401238a6c3df9d89ae0cfcba843efb14
Serega Bayern <pechkurovsg@mail.ru>
2024-11-20 13:03:22 +0300

    код задачи

--- Program.cs
+++ Program.cs
@@ -1,1 +1,23 @@
 ﻿
+string[] array = new string[] { "Russia", "Denmark", "Kazan" };
+
+int lenNewArray = 0;
+for (int i = 0; i <= array.Length - 1; i++)
+{
+    if (array[i].Length <= 3)
+        lenNewArray++;
+}
+
+string[] newArray = new string[lenNewArray];
+int idx = 0;
+Console.Write("[");
+for (int i = 0; i <= array.Length - 1; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        newArray[idx] = array[i];
+        Console.Write($"{newArray[idx]}, ");
+        idx++;
+    }
+}
+Console.Write("]");
--- obj/Debug/net8.0/Control-work.AssemblyInfo.cs
+++ obj/Debug/net8.0/Control-work.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Control-work")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+01b26a2cdf70ed962a40c1336a09f1257a6fb44f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Control-work")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Control-work")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
